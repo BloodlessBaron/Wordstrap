b529d4b1dd6ef67b0721cffb2d1f9f8e27f2f8dd
John Barner <bloodlessbaron@protonmail.com>
2018-10-25 22:17:59 -0500

    widget support added to sidebar

--- single.php
+++ single.php
@@ -6,6 +6,8 @@
         while ( have_posts() ) : the_post();
       ?>
         <div id="post-<?php the_ID(); ?>" <?php post_class( 'class-name' ); ?>>
+          <h2 class="blog-post-title"><?php the_title(); ?></h2>
+          <p class="text-muted"><?php the_date(); ?> by <?php the_author(); ?> in <cite title="The Category"><?php the_category(', '); ?></p>
           <?php the_content(); ?>
         </div><!-- /.blog-post -->
         <?php
