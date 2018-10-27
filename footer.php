b3790c3c2d559463d5cd917dfca70edb7a220d4e
John Barner <bloodlessbaron@protonmail.com>
2018-10-27 14:40:00 -0500

    fixed missing ul

--- footer.php
+++ footer.php
@@ -3,11 +3,13 @@
 
     <footer class="footer bg-dark">
       <div id="footer" class="container text-muted">
-        <?php if ( is_active_sidebar( 'footer' ) ) {
-          dynamic_sidebar( 'footer' );
-        } else { ?>
-        &copy; <?php echo date("Y"). ' ' . get_bloginfo('name'); ?>.
-      <?php } ?>
+        <ul>
+          <?php if ( is_active_sidebar( 'footer' ) ) {
+              dynamic_sidebar( 'footer' );
+            } else { ?>
+            &copy; <?php echo date("Y"). ' ' . get_bloginfo('name'); ?>.
+          <?php } ?>
+        </ul>
       </div>
     </footer>
 
