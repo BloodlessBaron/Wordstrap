fda99d22e27909997f1c31e20db634bcfa7c8227
John Barner <bloodlessbaron@protonmail.com>
2018-10-27 22:34:06 -0500

    fixed accidently removed mobile menu button

--- header.php
+++ header.php
@@ -12,6 +12,9 @@
       <!-- Fixed navbar -->
       <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
         <a class="navbar-brand" href="<?php echo get_bloginfo( 'wpurl' );?>"><?php echo get_bloginfo( 'name' ); ?></a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
         <div class="collapse navbar-collapse" id="navbarCollapse">
           <?php wp_nav_menu( array(
           'theme_location' => 'header-menu',
